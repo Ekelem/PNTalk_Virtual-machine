164787fd0d2346a7efc405b18a007463ab2136e3
Erik Kelemen <xkelem01@stud.fit.vutbr.cz>
2020-07-30 04:36:40 +0200

    chore: proto files

--- protos/build-protobuf.sh
+++ protos/build-protobuf.sh
@@ -5,6 +5,6 @@ mkdir build
 echo "build protobuf directory cleared!"
 for filename in v3/*.proto; do
 	./protoc -I ../protos/v3 --cpp_out=build ../protos/$filename
-	./protoc -I ../protos/v3 --grpc_out=build --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ../protos/$filename
+	./protoc -I ../protos/v3 --grpc_out=build --plugin=protoc-gen-grpc=./grpc_cpp_plugin ../protos/$filename
 	echo "$filename generated!"
 done
